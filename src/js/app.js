c1aea6f06459911d26e77218b8da86aa516a850a
Hoangle1072003 <hoangkinboy@gmail.com>
2023-04-05 23:34:08 +0700

    them phan thuc don ca phe

--- src/js/app.js
+++ src/js/app.js
@@ -34,24 +34,43 @@ window.addEventListener('beforeunload', (event) => {
   const hiddenElements_Y = document.querySelectorAll('.hidden-Y');
   hiddenElements_Y.forEach((el) => observer_Y.observe(el));
   
-  var slideIndex = 0;
-  showSlides();
-  
+  var slideIndex;
+  // KHai bào hàm hiển thị slide
   function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000);
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+ 
+      slides[slideIndex].style.display = "block";  
+      dots[slideIndex].className += " active";
+      //chuyển đến slide tiếp theo
+      slideIndex++;
+      //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+      if (slideIndex > slides.length - 1) {
+        slideIndex = 0
+      }    
+      //tự động chuyển đổi slide sau 5s
+      setTimeout(showSlides, 5000);
   }
+  //mặc định hiển thị slide đầu tiên 
+  showSlides(slideIndex = 0);
+ 
+ 
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+
+
+const carousel = document.querySelector(".menucaphe__item")
+const dragging =(e)=>{
+  carousel.scrollLeft= e.pageX;
+}
+
+ carousel.addEventListener("mousemove",dragging)
