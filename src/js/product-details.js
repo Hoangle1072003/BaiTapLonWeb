16704cb9fa1b1f6c386f62ba4629a7363b30fbe4
Hoangle1072003 <hoangkinboy@gmail.com>
2023-04-27 17:14:38 +0700

    add file HomePage

--- src/js/product-details.js
+++ src/js/product-details.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
     let id = document.URL.slice(-2);
     $.getJSON('/products.json', function(data){
-        let oderdetail = data[2].freeze;
+        let oderdetail = data[6].chung;
         $(".orderdetail__container").html(
             `
             <div class="oderdetail__heading"> 
@@ -21,87 +21,35 @@ jQuery(document).ready(function($){
             </div>
           </div>
         </div>
-            `
-        )
-
-        //
-
-
-        let caphe = data[0].caphe;
-        $(".orderdetail__container").html(
-            `
-            <div class="oderdetail__heading"> 
-          <h1>${caphe[id-1].title}</h1>
-        </div>
-        <div class="oderdetail__grip"> 
-          <div class="oderdetail__row"> 
-            <div class="oderdetail__img"> <img src=${caphe[id-1].img} alt=${caphe[id-1].title}></div>
-            <div class="oderdetail__content"> 
-              <div class="oderdetail__content--text"> 
-                <p>${caphe[id-1].title2}</p>
-              </div>
-              <div class="oderdetail__content--btn"><a href= ${caphe[id-1].url}>ĐẶT MUA NGAY</a></div>
-              <div class="oderdetail__content--price"> 
-                <h1>Giá:<span>${caphe[id-1].price}</span></h1>
-              </div>
-            </div>
-          </div>
-        </div>
-            `
-        )
-
-        //
-        let tra = data[0].tra;
-        $(".orderdetail__container").html(
-            `
-            <div class="oderdetail__heading"> 
-          <h1>${tra[id-1].title}</h1>
-        </div>
-        <div class="oderdetail__grip"> 
-          <div class="oderdetail__row"> 
-            <div class="oderdetail__img"> <img src=${tra[id-1].img} alt=${tra[id-1].title}></div>
-            <div class="oderdetail__content"> 
-              <div class="oderdetail__content--text"> 
-                <p>${tra[id-1].title2}</p>
-              </div>
-              <div class="oderdetail__content--btn"><a href= ${tra[id-1].url}>ĐẶT MUA NGAY</a></div>
-              <div class="oderdetail__content--price"> 
-                <h1>Giá:<span>${tra[id-1].price}</span></h1>
-              </div>
-            </div>
-          </div>
-        </div>
-            `
-        )
-
-
-        //
-
-
-        let khac = data[0].khac;
-        $(".orderdetail__container").html(
-            `
-            <div class="oderdetail__heading"> 
-          <h1>${khac[id-1].title}</h1>
-        </div>
-        <div class="oderdetail__grip"> 
-          <div class="oderdetail__row"> 
-            <div class="oderdetail__img"> <img src=${khac[id-1].img} alt=${khac[id-1].title}></div>
-            <div class="oderdetail__content"> 
-              <div class="oderdetail__content--text"> 
-                <p>${khac[id-1].title2}</p>
-              </div>
-              <div class="oderdetail__content--btn"><a href= ${khac[id-1].url}>ĐẶT MUA NGAY</a></div>
-              <div class="oderdetail__content--price"> 
-                <h1>Giá:<span>${khac[id-1].price}</span></h1>
-              </div>
-            </div>
-          </div>
-        </div>
-            `
-        )
+            `)
     })
-   
 })
 
+
+
+// let id = document.URL.slice(-2);
+// console.log(id);
+// $.getJSON('/products.json', function(data){
+//     let oderdetail = data[1].tra;
+//     let currentItem = oderdetail[id-1];
+//     let htmlString = `
+//         <div class="oderdetail__heading"> 
+//             <h1>${currentItem.title}</h1>
+//         </div>
+//         <div class="oderdetail__grip"> 
+//             <div class="oderdetail__row"> 
+//                 <div class="oderdetail__img"> <img src=${currentItem.img} alt=${currentItem.title}></div>
+//                 <div class="oderdetail__content"> 
+//                     <div class="oderdetail__content--text"> 
+//                         <p>${currentItem.title2}</p>
+//                     </div>
+//                     <div class="oderdetail__content--btn"><a href= ${currentItem.url}>ĐẶT MUA NGAY</a></div>
+//                     <div class="oderdetail__content--price"> 
+//                         <h1>Giá:<span>${currentItem.price}</span></h1>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>`;
+//     $(".orderdetail__container").html(htmlString);
+// });
 
